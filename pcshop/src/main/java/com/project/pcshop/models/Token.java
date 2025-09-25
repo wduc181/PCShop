15425dff7868c6163b7df48fe7429c524934422c
Vo Viet Duc <vvd1811@gmail.com>
2025-09-25 13:37:35 +0700

    feat(token model): added token model

---
+++ pcshop/src/main/java/com/project/pcshop/models/Token.java
@@ -0,0 +1,39 @@
+package com.project.pcshop.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Table(name = "tokens")
+@Data
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Token {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "token", unique = true, nullable = false)
+    private String token;
+
+    @Column(name = "token_type", nullable = false, length = 50)
+    private String tokenType;
+
+    @Column(name = "expiration_date")
+    private LocalDateTime expirationDate;
+
+    @Column(name = "revoked", nullable = false)
+    private Boolean revoked;
+
+    @Column(name = "expired", nullable = false)
+    private Boolean expired;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
