f12c1ed4a75ffe99019972117006db9fb304a54a
wduc181 <vvd1811@gmail.com>
2026-01-05 11:28:32 +0700

    fix: SerializationException in get data from cache

--- pcshopbackend/pcshop/src/main/java/com/project/pcshop/configurations/RedisConfig.java
+++ pcshopbackend/pcshop/src/main/java/com/project/pcshop/configurations/RedisConfig.java
@@ -5,8 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
 @EnableCaching
@@ -14,19 +16,19 @@ public class RedisConfig {
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(
-            RedisConnectionFactory connectionFactory
+            RedisConnectionFactory connectionFactory,
+            ObjectMapper objectMapper
     ) {
+        Jackson2JsonRedisSerializer<Object> json = new Jackson2JsonRedisSerializer<>(objectMapper, Object.class);
+
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
-
         template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(json);
         template.setHashKeySerializer(new StringRedisSerializer());
-
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
-
+        template.setHashValueSerializer(json);
         template.afterPropertiesSet();
         return template;
-    }
+        }
 }
 
